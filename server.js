747a590d4c4d61b41bb4f1ef93627ba26ff6b880
christopherlinford <linfordc05@gmail.com>
2025-11-16 16:41:24 -0700

    update files, remove video

--- server.js
+++ server.js
@@ -14,7 +14,36 @@ const baseController = require("./controllers/baseController.js")
 const inventoryRoute = require("./routes/inventoryRoute.js")
 const utilities =require("./utilities/index.js")
 const intentionalErrorRoute = require("./routes/intentionalErrorRoute.js");
-const pool = require("./database/index.js")
+//const pool = require("./database/index.js")
+const session = require("express-session")
+const pool = require('./database/')
+
+
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+
+
 
 
 
